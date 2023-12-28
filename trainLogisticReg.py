b5e3ce89d9a0434a87c7361108d1531a425f72ba
lulufufu666 <151918450+lulufufu666@users.noreply.github.com>
2023-12-28 22:26:23 +0800

    Update trainLogisticReg.py

--- trainLogisticReg.py
+++ trainLogisticReg.py
@@ -6,45 +6,46 @@ from sklearn.metrics import roc_auc_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+# Read the data
+filename = 'D:/train.csv'
+data = pd.read_csv(filename)
 
-filename = 'D:\\train.csv'
-datas = pd.read_csv(filename)
-header = datas.columns.tolist()  # 获取列名，即头信息
-header = header[1:len(header)-1:1]
+# Extract features and labels
+header = data.columns.tolist()
+header = header[1:len(header)-1:1]  # Get column names of features
 
-X = datas.drop('target', axis=1)
-y = datas['target']
+X = data.drop('target', axis=1)
+y = data['target']
 
+# Data preprocessing
 imputer = SimpleImputer(strategy='mean')
 X_imputed = imputer.fit_transform(X)
 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X_imputed)
 
+# Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
 
+# Train the logistic regression model
 logreg = LogisticRegression(C=100)
-
 logreg.fit(X_train, y_train)
 
-print(f"C in value: {logreg.C} >>>")
+# Model evaluation
+print(f"C value: {logreg.C}")
 accuracy = round(logreg.score(X_train, y_train), 4)
-print("Training Setting Accuracy: ", accuracy)
+print("Training set accuracy:", accuracy)
 accuracy = round(logreg.score(X_test, y_test), 4)
-print("Testing Setting Accuracy: ", accuracy)
-print("Accuracy on testing set: {:.3f}".format(roc_auc_score(y_test, logreg.predict(X_test))))
-print("classification report:\n", classification_report(y_test, logreg.predict(X_test),
-                                                        target_names=["非5g", "5g"]))
+print("Test set accuracy:", accuracy)
+print("Test set AUC value: {:.3f}".format(roc_auc_score(y_test, logreg.predict_proba(X_test)[:, 1])))
+print("Classification report:\n", classification_report(y_test, logreg.predict(X_test), target_names=["Non-5G", "5G"]))
 
-# 获取模型学习到的系数
+# Visualize feature coefficients
 coefs = logreg.coef_
-
-# 可视化系数
-plt.figure(figsize=(10, 6))  # 设置图表宽度为12，高度为6
+plt.figure(figsize=(10, 6))
 plt.bar(list(range(len(coefs[0]))), coefs[0])
-plt.xticks(list(range(58)), header, rotation=90)
-plt.xlabel("Coefficient index")
-plt.ylabel("Coefficient value")
-plt.title(f"Coefficients of features when 'C' is {logreg.C}")
+plt.xticks(list(range(len(coefs[0]))), header, rotation=90)
+plt.xlabel("Feature Index")
+plt.ylabel("Feature Coefficient Value")
+plt.title(f"Feature Coefficients when 'C' is {logreg.C}")
 plt.show()
-
